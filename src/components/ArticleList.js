005c3c604359c206c9cd33a58306dbf44bd401d0
Ian Wafula <neocorpse@proton.me>
2025-04-09 05:52:48 +0300

    Complete blog layout

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import blogData from '../data/blog';
+import Article from './Article';
+
+export default function ArticleList() {
+	return (
+		<main>
+			{blogData.posts.map((post) => (
+				<Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+			))}
+		</main>
+	);
+}
